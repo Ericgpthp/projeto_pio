072f9063f05657dc400de863c878b48cf42b4bc2
Gabriel Campanholi <biel.campanholi@hotmail.com>
2022-05-13 19:31:01 -0300

    Adição de classe

---
+++ projeto_pio/Dados.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace projeto_pio
+{
+    internal class Dados
+    {
+        public bool inserirUsuario(string nome, string email, string senha)
+        {
+            if (nome == "1")
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+
+
+        }
+    }
+}
